705889c145d9ce88c989d7509516f10c99688a0c
SolBumH <ppj2130@gmail.com>
2024-05-02 19:16:38 +0900

    Login 컨트롤러 생성

---
+++ src/main/java/com/team_3/controller/LoginContoller.java
@@ -0,0 +1,25 @@
+package com.team_3.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.team_3.dto.UserDTO;
+
+@Controller
+public class LoginContoller {
+
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+	@PostMapping("/login")
+	// public String login(@RequestParam(name = "id") String id, @RequestParam(name = "pw") String pw) {
+	public String login(UserDTO user) {
+		System.out.println("id : " + user.getLoginId());
+		System.out.println("pw : " + user.getPw());
+		return "redirect:/login";
+	}
+}
