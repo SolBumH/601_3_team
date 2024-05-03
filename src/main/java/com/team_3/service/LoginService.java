dbf73fdcebe89298213aa46600aa9d694f6a6495
SolBumH <ppj2130@gmail.com>
2024-05-03 16:20:03 +0900

    Login을 위한 JPA 메소드명 테스트

---
+++ src/main/java/com/team_3/service/LoginService.java
@@ -0,0 +1,21 @@
+package com.team_3.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.team_3.dto.UserDTO;
+import com.team_3.repository.LoginRepository;
+
+@Service
+public class LoginService {
+	
+	@Autowired
+	private LoginRepository loginRepository;
+
+	public List<UserDTO> findByLoginid(String loginid, String pw) {
+		return loginRepository.findByLoginidAndPw(loginid, pw);
+	}
+	
+}
