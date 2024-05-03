eed9ab1110087d1d929eebfc1109872e526d5b17
SolBumH <ppj2130@gmail.com>
2024-05-03 15:15:57 +0900

    Toast UI 테스트용

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -2,3 +2,12 @@ function test() {
   let id = document.getElementById("id");
   alert(id.value);
 }
+
+const editor = new toastui.Editor({
+  el: document.querySelector("#content"),
+  height: "600px",
+  initialEditType: "markdown",
+  previewStyle: "vertical",
+  initailValue: "",
+  usageStatistics: false,
+});
