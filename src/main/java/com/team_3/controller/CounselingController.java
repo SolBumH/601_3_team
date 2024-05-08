1cc210e24d0c6ccaa4ff6bb1e9d5dc8c90ea034e
SolBumH <ppj2130@gmail.com>
2024-05-08 17:03:30 +0900

    로그인 여부 체크하기
    UserUtil을 만들어 세션의 이름, 권한 확인하기

--- src/main/java/com/team_3/controller/CounselingController.java
+++ src/main/java/com/team_3/controller/CounselingController.java
@@ -1,55 +1,68 @@
 package com.team_3.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.team_3.util.UserUtil;
+
 @Controller
 public class CounselingController {
+	
+	@Autowired
+	private UserUtil userUtil;
 	
 	@GetMapping("srCounseling")
 	public String srCounseling(Model model) {
 		model.addAttribute("counselingTitle", "심리 상담 안내");
+		model.addAttribute("user", userUtil.getUserNameAndRole());
 		return "sangdam";
 	}
 	
 	@GetMapping("/jmCounseling")
 	public String jmCounseling(Model model) {
 		model.addAttribute("counselingTitle", "전문 상담 안내");
+		model.addAttribute("user", userUtil.getUserNameAndRole());
 		return "sangdam";
 	}
 	
 	@GetMapping("/jdCounseling")
 	public String jdCounseling(Model model) {
 		model.addAttribute("counselingTitle", "집단 상담 안내");
+		model.addAttribute("user", userUtil.getUserNameAndRole());
 		return "group";
 	}
 	
 	@GetMapping("/groupsangdam.html")
-	public String groupSangdam() {
-		return "groupsangdam.html";
+	public String groupSangdam(Model model) {
+		model.addAttribute("user", userUtil.getUserNameAndRole());
+		return "groupsangdam";
 	}
 	
 	@GetMapping("/groupDetail.html")
-	public String groupDetail() {
-		return "groupDetail.html";
+	public String groupDetail(Model model) {
+		model.addAttribute("user", userUtil.getUserNameAndRole());
+		return "groupDetail";
 	}
 	
 	@GetMapping("/groupRequest.html")
-	public String groupRequest() {
-		return "groupRequest.html";
+	public String groupRequest(Model model) {
+		model.addAttribute("user", userUtil.getUserNameAndRole());
+		return "groupRequest";
 	}
 	
 	@GetMapping("/jcCounseling")
 	public String jcCounseling(Model model) {
 		model.addAttribute("counselingTitle", "취업·진로 상담 안내");	
-		
+		model.addAttribute("user", userUtil.getUserNameAndRole());
 		return "jcCounseling";
 	}
 	
 	@GetMapping("/jkCounseling")
 	public String jkCounseling(Model model) {
 		model.addAttribute("counselingTitle", "교수 상담 안내");
+		model.addAttribute("user", userUtil.getUserNameAndRole());
 		return "jkCounseling";
 	}
 }
