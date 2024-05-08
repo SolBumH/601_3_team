8e7e7bd3376d225a228e01a831f640995ebb0e0f
faker <wlgns>
2024-05-08 17:38:27 +0900

    캘린더 날짜 클릭시 자바로 날짜보내기

--- src/main/java/com/team_3/controller/SrController.java
+++ src/main/java/com/team_3/controller/SrController.java
@@ -3,11 +3,21 @@ package com.team_3.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SrController {
 	@GetMapping("/srconsulting")
 	public String sd(Model model) {
 		return "srconsulting";
+	}
+	
+	@PostMapping("/SrController")
+	@ResponseBody
+	public String arg(@RequestBody String arg) {
+		System.out.println(arg);
+		return "srcunsulting";
 	}
 }
---
+++ src/main/resources/static/js/calendar.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function() {
+	let calendarEl = document.getElementById('calendar')
+	let calendar = new FullCalendar.Calendar(calendarEl, {
+
+		initialView: 'dayGridMonth', // 초기화면에 보이는 캘린더 화면
+		contentHeight: 600, //캘린더 크기 설정
+		locale: 'ko', // 한국어로 변경
+		aspectRatio: 1.35, // 가로세로 비율
+		selectable: true, // 달력 드래그 설정
+		dayMaxEventRows: true, // for all non-TimeGrid views
+		nowIndicator: true, // 현재 시간 마크
+		editable: true, // 기존 예약을 옮길수 있음
+		select: function(arg) {
+			click(arg.startStr);
+
+
+			calendar.unselect();
+		} //달력 빈칸클릭시 event 추가 가능
+	});
+
+	calendar.render();
+
+
+});
+function click(arg) {
+	$.ajax({
+		type: 'post',
+		url: '/SrController',
+		dataType: 'json',
+		data: {
+			start: arg,
+		},
+		success: function(data) {
+			console.log(data);
+		},
+		error: function(error) {
+			console.log(error);
+		}
+	});
+	alert(arg);
+}
