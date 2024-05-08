d6166ba1724279ac19b40c2517156fee368b9870
SolBumH <ppj2130@gmail.com>
2024-05-08 15:13:31 +0900

    spring security 로그인, 로그아웃 구현

--- src/main/java/com/team_3/config/SecurityConfig.java
+++ src/main/java/com/team_3/config/SecurityConfig.java
@@ -29,15 +29,28 @@ public class SecurityConfig {
         
         // 세션 중복 로그인 허용 여부
         http .sessionManagement((auth) -> auth
-                .maximumSessions(1)
+                .maximumSessions(1) // 다중 로그인 허용 개수
                 .maxSessionsPreventsLogin(true));
+        // true : 초과 시 새로운 로그인 차단
+        // false : 초과 시 기존 세션 삭제
         
         // 로그인이 안되어 오류 페이지 발생 시 로그인 페이지로 이동
         http.formLogin((auth) -> auth.loginPage("/login")
         		.loginProcessingUrl("/login")  // 로그인 시 해당 URL로 값 전송
+        		.defaultSuccessUrl("/")
         		.permitAll());
         
-        http.csrf((auth) -> auth.disable()); // 개발 중에는 잠시 꺼두기
+        // 세션 고정 보호, 10강
+        http.sessionManagement((auth) -> auth
+                .sessionFixation().changeSessionId());
+        
+        // 로그아웃 하기
+        http.logout((auth) -> auth
+        		.logoutUrl("/logout")
+        		.logoutSuccessUrl("/")
+        		.invalidateHttpSession(true));
+        
+        // http.csrf((auth) -> auth.disable()); // 개발 중에는 잠시 꺼두기
         
         return http.build();
     }
