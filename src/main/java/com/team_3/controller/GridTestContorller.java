7bd429ab50c6227a3ebfbc00e8044631e3fc59d0
faker <wlgns>
2024-05-08 14:08:57 +0900

    grid 테스트

--- src/main/java/com/team_3/controller/GridTestContorller.java
+++ src/main/java/com/team_3/controller/GridTestContorller.java
@@ -1,15 +1,12 @@
 package com.team_3.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class GridTestController {
-	@GetMapping("/gridtest")
-	
-	public String grid(Model model) {
+public class GridTestContorller {
+	@GetMapping("gridtest")
+	public String grid() {
 		return "gridtest";
 	}
-	
 }
