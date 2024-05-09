a9f81a10f33247e5b57c6932426fcef070937b5f
SolBumH <ppj2130@gmail.com>
2024-05-09 17:23:19 +0900

    전문상담페이지 템플릿 교체

--- src/main/java/com/team_3/config/SecurityConfig.java
+++ src/main/java/com/team_3/config/SecurityConfig.java
@@ -1,19 +1,11 @@
 package com.team_3.config;
 
-import java.io.IOException;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 @Configuration
 @EnableWebSecurity
