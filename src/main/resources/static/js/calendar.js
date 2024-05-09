c59a8ae534384f16ded9e6e7f7781210743023ad
SolBumH <ppj2130@gmail.com>
2024-05-09 19:19:46 +0900

    달력 2줄만 포시되게 설정, 달력 페이지 input 설정

--- src/main/resources/static/js/calendar.js
+++ src/main/resources/static/js/calendar.js
@@ -6,9 +6,9 @@ document.addEventListener('DOMContentLoaded', function() {
 	let calendar = new FullCalendar.Calendar(calendarEl, {
 
 		initialView: 'dayGridMonth', // 초기화면에 보이는 캘린더 화면
-		contentHeight: 600, //캘린더 크기 설정
+		contentHeight: 180, //캘린더 크기 설정
 		locale: 'ko', // 한국어로 변경
-		aspectRatio: 1.35, // 가로세로 비율
+		// aspectRatio: 1.35, // 가로세로 비율
 		selectable: true, // 달력 드래그 설정
 		// dayMaxEventRows: false, // for all non-TimeGrid views
 		nowIndicator: true, // 현재 시간 마크
