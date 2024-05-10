f5b3ec0e89278c4ae02feff0174b5f69c617920c
jaehee <njh07017@gmail.com>
2024-05-10 17:26:51 +0900

    상담신청폼 작성 완료 - db에 보내는 작업중

---
+++ src/main/java/com/team_3/repository/CounselingRepository.java
@@ -0,0 +1,17 @@
+package com.team_3.repository;
+
+import org.springframework.stereotype.Repository;
+
+import com.team_3.dto.CounselingFormDTO;
+
+@Repository
+public class CounselingRepository {
+
+	public void save(CounselingFormDTO form) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+
+
