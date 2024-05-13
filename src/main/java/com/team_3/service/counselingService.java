9dad14e32040c2253f008ef726451f1a5533f8ba
faker <wlgns>
2024-05-13 13:57:52 +0900

    dd

--- src/main/java/com/team_3/service/counselingService.java
+++ src/main/java/com/team_3/service/counselingService.java
@@ -1,10 +1,11 @@
 package com.team_3.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.team_3.dto.CounselingFormDTO;
 import com.team_3.repository.CounselingRepository;
-
+@Service
 public class counselingService {
 
 	  @Autowired
