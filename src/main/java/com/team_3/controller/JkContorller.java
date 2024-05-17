a20c4e9f95d3d6bf93d2315d066965194d081096
ki_ng <eowhd171@naver.com>
2024-05-17 17:48:59 +0900

    admin 상담관리 작업중

--- src/main/java/com/team_3/controller/JkContorller.java
+++ src/main/java/com/team_3/controller/JkContorller.java
@@ -1,12 +1,17 @@
 package com.team_3.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.team_3.dto.JkDTO;
 import com.team_3.service.JkService;
 import com.team_3.util.UserUtil;
 
@@ -22,14 +27,18 @@ public class JkContorller {
 	
 	@PostMapping("/JkControll")
 	@ResponseBody
-	public String JkControll(@RequestParam(name = "start") String start) {
+	public String JkControll(@RequestParam(name = "start") String start,Model model) {
 		System.out.println(start);
 		return "admin/adminJkCounseling";
 	}
 	
 	@PostMapping("/jkselect")
-	public String jkselect(@RequestParam(name = "jkselectdate") String jkselectdate) {
+	public String jkselect(@RequestParam(name = "jkselectdate") String jkselectdate,
+							@RequestParam(name = "jkoption") String jkoption, Model model, JkDTO dto) {
 		System.out.println(jkselectdate);
-		return "redirect:/admin/jkCounseling";
+		System.out.println(jkoption);
+		//List<Map<String, Object>> list = jkService.getJkList(jkselectdate);
+		//model.addAttribute("list",list);
+		return "redirect:/admin/adminJkCounseling";
 	}
 }
--- src/main/java/com/team_3/service/JkService.java
+++ src/main/java/com/team_3/service/JkService.java
@@ -1,5 +1,8 @@
 package com.team_3.service;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,5 +13,10 @@ public class JkService {
 	
 	@Autowired
 	private JkRepository jkRepository;
+
+	public List<Map<String, Object>> getJkList(String jkselectdate) {
+		
+		return jkRepository.getJkList(jkselectdate);
+	}
 
 }
