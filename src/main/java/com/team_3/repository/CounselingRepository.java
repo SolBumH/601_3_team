2e2c294f5dfb57892da3f081bf8533b5be95fc33
hyojin <jhj4591@naver.com>
2024-05-20 18:16:38 +0900

    집단상담 신청완료 페이지 이름 제외 값 불러옴.

--- src/main/java/com/team_3/repository/CounselingRepository.java
+++ src/main/java/com/team_3/repository/CounselingRepository.java
@@ -11,8 +11,6 @@ public interface CounselingRepository {
 	
 	 void saveForm(CounselingDTO formDTO);
 	 
-	 List<BoardDTO> getGroupData();
-
 	List<BoardDTO> getGroupData();
 
 	BoardDTO getDetail(int no);
