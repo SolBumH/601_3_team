38ccd126735ae425278eefd77ee88c26f7737b2a
hyojin <jhj4591@naver.com>
2024-05-21 18:03:31 +0900

    관리자페이지 집단상담 결과 부분 삭제함

--- src/main/java/com/team_3/controller/AdminController.java
+++ src/main/java/com/team_3/controller/AdminController.java
@@ -4,7 +4,6 @@ import java.text.ParseException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,11 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.team_3.dto.BoardDTO;
 import com.team_3.dto.CounselingFormDTO;
-import com.team_3.dto.GroupDTO;
 import com.team_3.dto.UserDTO;
 import com.team_3.service.AdminService;
 import com.team_3.util.UserUtil;
@@ -79,26 +76,6 @@ public class AdminController {
 	public String adminGroupAp(Model model) {
 		model.addAttribute("user", userUtil.getUserNameAndRole());
 		return "/admin/adminGroupAp";
-		
-	}
-	
-	@PostMapping("/saveGroupForm")
-	public String saveGroupForm(@RequestParam(name = "programName") String programName,
-								@RequestParam(name = "date") @DateTimeFormat(pattern = "yyyy-MM-dd") Date date,
-					            @RequestParam(name = "endDate") @DateTimeFormat(pattern = "yyyy-MM-dd") Date endDate,
-								@RequestParam("selectedType") String selectedType,
-                                @RequestParam("programContent") String programContent,
-                                RedirectAttributes redirectAttributes) {
-									
-		GroupDTO groupDTO = new GroupDTO();
-		groupDTO.setPG_NAME(programName);
-		groupDTO.setJMS_CS_DATE(date);
-		groupDTO.setEN_DATE(endDate);
-		groupDTO.setPG_EXPLAIN(programContent);
-		
-		adminService.save(groupDTO);
-		
-		return "redirect:/admin/adminGroup";
 		
 	}
 	
--- src/main/java/com/team_3/service/AdminService.java
+++ src/main/java/com/team_3/service/AdminService.java
@@ -211,4 +211,6 @@ public class AdminService {
 		
 		return result;
 	}
+
+
 }
